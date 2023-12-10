cf86fe9e4b293ef4798461e93e0f4ae592131f51
arhian <arhianalbis7@gmail.com>
2023-12-10 13:50:33 -0600

    Finish UI, add multiplayer capabilities, improve all pages, wire API calls to the backend correctly.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import logo from './logo.svg';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Settings from './Pages/Settings';
+import Game from './Pages/Game';
+import Home from './Pages/Home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Routes>
+      <Route path='/'element={<Home />} /> 
+      <Route path='settings' element={<Settings />} />
+      <Route path='game' element={<Game />} />
+
+    </Routes>
   );
 }
 
